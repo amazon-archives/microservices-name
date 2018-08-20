dd7885c712ce69dfc586137bb1e5ed1dfaaf5130
Christoph Kassen <christoph@kassen.name>
2018-08-20 19:05:27 +0200

    Throw random errors
    Throw random errors to demonstrate tracing capabilities.

--- src/main/java/org/aws/samples/compute/name/NameEndpoint.java
+++ src/main/java/org/aws/samples/compute/name/NameEndpoint.java
@@ -7,33 +7,50 @@ import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.ServiceUnavailableException;
+import javax.ws.rs.ClientErrorException;
+import java.util.Random;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * @author Arun Gupta
  */
 @Path("names")
 public class NameEndpoint {
+
+    private static final Logger logger = LoggerFactory.getLogger(NameHandler.class);
+
     @GET
     @Produces({MediaType.APPLICATION_XML + "; qs=0.50",
             MediaType.APPLICATION_JSON + "; qs=0.75"})
     public Name[] get() {
-        if (AWSXRay.getGlobalRecorder().getTraceEntity() != null)
-            AWSXRay.getCurrentSegment().putAnnotation("parentId",
-                AWSXRay.getGlobalRecorder().getTraceEntity().getId());
-
         return Names.findAll();
     }
-    
+
     @GET
     @Path("{id}")
     @Produces({MediaType.APPLICATION_XML + "; qs=0.50",
             MediaType.APPLICATION_JSON + "; qs=0.75",
             MediaType.TEXT_PLAIN + "; qs=1.0"})
     public Name get(@PathParam("id") int id) {
-        if (AWSXRay.getGlobalRecorder().getTraceEntity() != null)
-            AWSXRay.getCurrentSegment().putAnnotation("parentId",
-                AWSXRay.getGlobalRecorder().getTraceEntity().getId());
 
-        return Names.findById(id);
+        Random rand = new Random();
+        int n = rand.nextInt(100) + 1;
+
+        logger.info("Random is: " + n + " IsEven is:" + isEven);
+
+        if (n <= 80) {
+          return Names.findById(id);
+        } else {
+          if ((n % 2) == 0) {
+            throw new ServiceUnavailableException("Service not available", 30L);
+          } else {
+            throw new ClientErrorException(403);
+          }
+        }
+
+        //return Names.findById(id);
     }
 }
